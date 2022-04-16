71a5dea9d27e6126830390fbd37590e2553aa1d9
BlazerHeat <vghoghari82@gmail.com>
2022-04-17 00:25:58 +0530

    Edited Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,11 +2,48 @@ import styles from "../styles/components/Footer.module.scss";
 
 function Footer() {
     return (
-        <footer className={styles.footer}>
-            <p>
-                &copy; CharusatForum, All rights reserved -{" "}
-                {new Date().getFullYear()}.
-            </p>
+        <footer className={styles["footer"]}>
+            <div className={styles["footer-container"]}>
+                <div className={styles["social-links"]}>
+                    <a href="#" className={styles["footer-link"]}>
+                        <i className="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" className={styles["footer-link"]}>
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                    <a href="#" className={styles["footer-link"]}>
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" className={styles["footer-link"]}>
+                        <i className="fab fa-discord"></i>
+                    </a>
+                </div>
+                <hr />
+                <div className={styles["copyright-text"]}>
+                    <p>
+                        &copy; iCoder, All rights reserved -{" "}
+                        {new Date().getFullYear()}.
+                    </p>
+                </div>
+                <hr />
+                <div className={styles["quick-links"]}>
+                    <a href="/developers" className={styles["quick-link"]}>
+                        Developers
+                    </a>
+                    <span className={styles["bullet"]}>&bull;</span>
+                    <a href="/carriers" className={styles["quick-link"]}>
+                        Carriers
+                    </a>
+                    <span className={styles["bullet"]}>&bull;</span>
+                    <a href="/contact" className={styles["quick-link"]}>
+                        About
+                    </a>
+                    <span className={styles["bullet"]}>&bull;</span>
+                    <a href="/faq" className={styles["quick-link"]}>
+                        FAQs
+                    </a>
+                </div>
+            </div>
         </footer>
     );
 }
